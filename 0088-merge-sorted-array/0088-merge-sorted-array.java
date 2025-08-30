4d09e7d80ef735c0e5c249cce84832ce43586d63
Nishant Arun <nishantarun2512@gmail.com>
2025-08-30 20:31:01 +0530

    Time: 0 ms (100%), Space: 42.4 MB (42.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,46 +1,24 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] ans = mergeIt(nums1, m, nums2, n);
-        for(int i = 0; i< m+n; i++) {
-            nums1[i] = ans[i];
-        }
-    }
+        int[] merged = new int[m + n];
+        int i = 0, left = 0, right = 0;
 
-    public int[] mergeIt(int[] nums1, int m, int[] nums2, int n){
-        int[] res = new int[m+n];
-        int i =0;
-        int left = 0;
-        int right = 0;
-        while(left<m && right < n) {
-            if(nums1[left] < nums2[right]){
-                res[i] = nums1[left];
-                left++;
-                i++;
-            } 
-            else if(nums1[left] > nums2[right]){
-                res[i] = nums2[right];
-                i++;
-                right++;
-            }
-            else {
-                res[i] = nums1[left];
-                i++;
-                res[i] = nums2[right];
-                i++;
-                left++;
-                right++;
+        while (left < m && right < n) {
+            if (nums1[left] <= nums2[right]) {
+                merged[i++] = nums1[left++];
+            } else {
+                merged[i++] = nums2[right++];
             }
         }
-        while(left < m) {
-            res[i] = nums1[left];
-            i++;
-            left++;
+        while (left < m) {
+            merged[i++] = nums1[left++];
         }
-        while(right < n) {
-            res[i] = nums2[right];
-            i++;
-            right++;
+        while (right < n) {
+            merged[i++] = nums2[right++];
         }
-        return res;
+
+        for (int j = 0; j < m + n; j++) {
+            nums1[j] = merged[j];
+        }
     }
 }
