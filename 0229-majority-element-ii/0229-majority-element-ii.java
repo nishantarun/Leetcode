fb5039ddaf42b1912171a4dde4fa2c99b69bf24e
Nishant Arun <nishantarun2512@gmail.com>
2025-09-02 00:30:03 +0530

    Time: 2 ms (99.8%), Space: 50.2 MB (67.9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,27 +1,38 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // Create a frequency map to store the count of each element
-        Map<Integer, Integer> elementCountMap = new HashMap<>();
-        
-        // Iterate through the input array to count element occurrences
-        for (int i = 0; i < nums.length; i++) {
-            elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        List<Integer> majorityElements = new ArrayList<>();
-        int threshold = nums.length / 3;
-        
-        // Iterate through the frequency map to identify majority elements
-        for (Map.Entry<Integer, Integer> entry : elementCountMap.entrySet()) {
-            int element = entry.getKey();
-            int count = entry.getValue();
-            
-            // Check if the element count is greater than the threshold
-            if (count > threshold) {
-                majorityElements.add(element);
+        int n = nums.length;
+        // There can be at most two majority elements
+        int count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 1;
+
+        // Phase 1: Find potential candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
         }
-        
-        return majorityElements;
+
+        // Phase 2: Verify the candidates
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        if (count1 > n / 3) res.add(candidate1);
+        if (count2 > n / 3) res.add(candidate2);
+        return res;
     }
 }
