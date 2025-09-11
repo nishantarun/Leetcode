81cbb185f4a2f977d020bcd4368248b7c1ec81f8
Nishant Arun <nishantarun2512@gmail.com>
2025-09-11 21:48:05 +0530

    Time: 1 ms (84.34%), Space: 41.2 MB (30.7%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x == 0) {
-            return 0;
+        if(x == 0 || x == 1) {
+            return x;
         }
         int ans = helper(x, 1, x);
         return ans;
