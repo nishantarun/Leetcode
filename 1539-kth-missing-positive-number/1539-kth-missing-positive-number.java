1fe93d9fdbfa73ada4dd6f22db2484eedacb81c0
Nishant Arun <nishantarun2512@gmail.com>
2025-09-23 15:59:03 +0530

    Time: 0 ms (100%), Space: 42.8 MB (58.83%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,34 +1,22 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int missingCount = 0;
-        int num = 1;
-        while(missingCount <= k){
-            if(!isFound(arr, num)){
-                missingCount++;
-                if(missingCount == k){
-                    return num;
-                }
-            }
-            num++;
-        }
-        return num;
-    }
+        int left = 0, right = arr.length - 1;
 
-    public boolean isFound(int[] arr, int num){
-        int start = 0;
-        int end = arr.length-1;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(arr[mid] == num) {
-                return true;
-            }
-            else if (arr[mid] < num) {
-                start = mid + 1;
-            }
-            else {
-                end = mid - 1;
+        // Binary search to find the position where missing count is >= k
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int missing = arr[mid] - (mid + 1);  // Missing numbers before arr[mid]
+
+            if (missing < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return false;
+
+        // Left is the insertion point where missing numbers passed k
+        // The k-th missing number is k + left because before index left,
+        // there are 'left' numbers present, so k is offset by 'left'.
+        return k + left;
     }
 }
