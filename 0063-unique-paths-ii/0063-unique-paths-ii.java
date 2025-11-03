81b9e69b35c3428b99266b7e88d5b3ad613919d4
Nishant Arun <nishantarun2512@gmail.com>
2025-11-03 14:52:54 +0530

    Time: 0 ms (100%), Space: 43.6 MB (7.76%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,42 +1,24 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        return maze(grid, 0, 0, grid.length, grid[0].length, dp);
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        return maze(grid, 0, 0, m, n, dp);
     }
 
     public static int maze(int[][] grid, int row, int col, int m, int n, int[][] dp) {
-        if (grid[0][0] == 1 || grid[m - 1][n - 1] == 1) {
+        if (row >= m || col >= n || grid[row][col] == 1)
             return 0;
-        }
-        if (dp[row][col] != 0) {
-            return dp[row][col];
-        }
-        if (row == m - 1 && col == n - 1) {
+        if (row == m - 1 && col == n - 1)
             return 1;
-        }
-        int x = 0, y = 0;
 
-        if (isSafeRight(grid, row, col, m, n)) {
-            x = maze(grid, row, col + 1, m, n, dp);
-        }
-        if (isSafeDown(grid, row, col, m, n)) {
-            y = maze(grid, row + 1, col, m, n, dp);
-        }
+        if (dp[row][col] != -1)
+            return dp[row][col];
 
-        return dp[row][col] = x + y;
-    }
+        int right = maze(grid, row, col + 1, m, n, dp);
+        int down = maze(grid, row + 1, col, m, n, dp);
 
-    public static boolean isSafeRight(int[][] grid, int row, int col, int m, int n) {
-        if (col < n - 1 && grid[row][col + 1] != 1) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isSafeDown(int[][] grid, int row, int col, int m, int n) {
-        if (row < m - 1 && grid[row + 1][col] != 1) {
-            return true;
-        }
-        return false;
+        return dp[row][col] = right + down;
     }
 }
