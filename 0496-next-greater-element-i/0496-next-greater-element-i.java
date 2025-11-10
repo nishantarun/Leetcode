4dcc08648ec5ce0ebc8a737fd239b740836ccab9
Nishant Arun <nishantarun2512@gmail.com>
2025-11-10 12:08:07 +0530

    Time: 2 ms (97.82%), Space: 45.3 MB (6.93%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,30 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> stack = new Stack<>();
-        int[] res = new int[nums2.length];
-        nextGreaterElement(nums2, nums2.length-1, stack, res);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        nextGreaterElement(nums2, nums2.length - 1, stack, map);
         int[] ans = new int[nums1.length];
-        int j = 0;
-        for(int ele : nums1) {
-            for(int i = 0; i< nums2.length; i++) {
-                if(nums2[i] == ele) {
-                    ans[j] = res[i];
-                    j++;
-                }
-            }
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
         }
         return ans;
     }
 
-    public void nextGreaterElement(int[] nums, int idx, Stack<Integer> stack, int[] res) {
+    public void nextGreaterElement(int[] nums, int idx, Stack<Integer> stack, HashMap<Integer, Integer> map) {
         if (idx < 0) {
             return;
         }
         while (!stack.isEmpty() && nums[idx] >= stack.peek()) {
             stack.pop();
         }
-        res[idx] = stack.isEmpty() ? -1 : stack.peek();
+        if (stack.isEmpty()) {
+            map.put(nums[idx], -1);
+        } else {
+            map.put(nums[idx], stack.peek());
+        }
         stack.push(nums[idx]);
-        nextGreaterElement(nums, idx - 1, stack, res);
+        nextGreaterElement(nums, idx - 1, stack, map);
     }
 }
