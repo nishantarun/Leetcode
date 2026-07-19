ea1aad906875b0437a398f124b0783f2f6080d50
Nishant Arun <nishantarun2512@gmail.com>
2026-07-20 00:17:08 +0530

    Time: 4 ms (74.44%), Space: 45.3 MB (36.67%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int[] nextGreaterArr = nextGreaterHelper(nums2);
-        for (int i = 0; i < nums2.length; i++) {
-            map.put(nums2[i], nextGreaterArr[i]);
-        }
-        int[] ans = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            ans[i] = map.get(nums1[i]);
-        }
-        return ans;
-    }
-
-    public int[] nextGreaterHelper(int[] arr) {
-        int[] res = new int[arr.length];
-        Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = arr.length - 1; i >= 0; i--) {
-            while (!dq.isEmpty() && dq.peek() <= arr[i]) {
-                dq.pop();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
             }
-            if (dq.isEmpty()) {
-                res[i] = -1;
+            if (stack.isEmpty()) {
+                map.put(nums2[i], -1);
             } else {
-                res[i] = dq.peek();
+                map.put(nums2[i], stack.peek());
             }
-            dq.push(arr[i]);
+            stack.push(nums2[i]);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.get(nums1[i]);
         }
         return res;
     }
