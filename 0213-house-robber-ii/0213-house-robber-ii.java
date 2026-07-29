1d4989418125cf87171ffbd02445f390f65a2802
Nishant Arun <nishantarun2512@gmail.com>
2026-07-29 12:36:03 +0530

    Time: 0 ms (100%), Space: 42.7 MB (61.2%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return nums[0];
-        int notTakingLast = helper(nums, 0, n - 2);
-        int notTakingFirst = helper(nums, 1, n - 1);
-        return Math.max(notTakingFirst, notTakingLast);
+        if(nums.length == 1) return nums[0];
+        int left = robLinear(nums, 0, nums.length - 2);
+        int right = robLinear(nums, 1, nums.length - 1);
+        return Math.max(left, right);
     }
 
-    public int helper(int[] nums, int start, int end) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return solve(nums, start, end, dp);
-    }
-
-    public int solve(int[] nums, int start, int end, int[] dp){
-        if(end < start) return 0;
-        if(end == start) return nums[start];
-        if(dp[end] != -1) return dp[end];
-        int take = nums[end] + solve(nums, start, end - 2, dp);
-        int notTake = solve(nums, start, end - 1, dp);
-        return dp[end] = Math.max(take, notTake);
+    public int robLinear(int[] nums, int start, int end) {
+        int prev = nums[start];
+        int prev2 = 0;
+        for(int i = start + 1; i <= end; i++) {
+            int take = nums[i];
+            if(i > 1) take += prev2;
+            int notTake = prev;
+            int curr = Math.max(take, notTake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 }
