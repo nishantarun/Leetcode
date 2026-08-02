8c0e004c65b774cb7f2f28f010cdee71fd53060a
Nishant Arun <nishantarun2512@gmail.com>
2026-08-02 19:16:43 +0530

    Time: 0 ms (100%), Space: 43.4 MB (65.31%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,24 +1,29 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-        return maze(grid, 0, 0, m, n, dp);
-    }
-
-    public static int maze(int[][] grid, int row, int col, int m, int n, int[][] dp) {
-        if (row >= m || col >= n || grid[row][col] == 1)
-            return 0;
-        if (row == m - 1 && col == n - 1)
-            return 1;
-
-        if (dp[row][col] != -1)
-            return dp[row][col];
-
-        int right = maze(grid, row, col + 1, m, n, dp);
-        int down = maze(grid, row + 1, col, m, n, dp);
-
-        return dp[row][col] = right + down;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    dp[0][0] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if (i > 0) {
+                    up = dp[i - 1][j];
+                }
+                if (j > 0) {
+                    left = dp[i][j - 1];
+                }
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
