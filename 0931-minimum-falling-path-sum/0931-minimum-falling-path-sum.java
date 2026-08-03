a5b827d64240525495e997885cf03498729738b0
Nishant Arun <nishantarun2512@gmail.com>
2026-08-03 18:54:32 +0530

    Time: 8 ms (28.23%), Space: 47.2 MB (63.78%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,31 +1,37 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int min = Integer.MAX_VALUE;
         int[][] dp = new int[n][n];
         for (int[] row : dp) {
             Arrays.fill(row, Integer.MAX_VALUE);
         }
-        for (int i = n - 1; i >= 0; i--) {
-            min = Math.min(min, solve(matrix, n - 1, i, dp));
-        }
-        return min;
-    }
 
-    public int solve(int[][] matrix, int row, int col, int[][] dp) {
-        if (col < 0 || col > matrix.length - 1) {
-            return (int) 1e9;
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = matrix[0][i];
         }
-        if (row == 0) {
-            return matrix[0][col];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int min = matrix[i][j];
+                int leftD = Integer.MAX_VALUE;
+                if (j > 0) {
+                    leftD = dp[i - 1][j - 1];
+                }
+                int up = dp[i - 1][j];
+                int rightD = Integer.MAX_VALUE;
+                if (j < n - 1) {
+                    rightD = dp[i - 1][j + 1];
+                }
+                min += Math.min(leftD, Math.min(up, rightD));
+                dp[i][j] = min;
+            }
         }
-        if (dp[row][col] != Integer.MAX_VALUE)
-            return dp[row][col];
-        int min = matrix[row][col];
-        int leftD = solve(matrix, row - 1, col - 1, dp);
-        int up = solve(matrix, row - 1, col, dp);
-        int rightD = solve(matrix, row - 1, col + 1, dp);
-        min += Math.min(leftD, Math.min(up, rightD));
-        return dp[row][col] = min;
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, dp[n - 1][i]);
+        }
+        
+        return min;
     }
 }
