bbbf69bac822cdeb994e4a67f621ab4412088e01
Nishant Arun <nishantarun2512@gmail.com>
2026-09-14 11:46:35 +0530

    Time: 4 ms (44.56%), Space: 51.8 MB (93.95%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -4,14 +4,14 @@ class Solution {
         int flips = 0;
         int left = 0;
         for (int right = 0; right < nums.length; right++) {
-            while (nums[right] == 0 && flips == k) {
+            if (nums[right] == 0) {
+                flips++;
+            }
+            while (flips > k) {
                 if (nums[left] == 0) {
                     flips--;
                 }
                 left++;
-            }
-            if (nums[right] == 0) {
-                flips++;
             }
             int length = right - left + 1;
             maxLength = Math.max(maxLength, length);
