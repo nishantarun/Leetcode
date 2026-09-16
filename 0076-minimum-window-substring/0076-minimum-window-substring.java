2cb0f9f99de4a2e4a388f1d7dc0266eeb9cb3933
Nishant Arun <nishantarun2512@gmail.com>
2026-09-16 19:00:04 +0530

    Time: 2 ms (99.68%), Space: 45.2 MB (93.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,18 +1,28 @@
 class Solution {
+
     public String minWindow(String s, String t) {
         int length = Integer.MAX_VALUE;
         int lIdx = 0;
         int rIdx = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+
+        int[] map = new int[128];
         for (int i = 0; i < t.length(); i++) {
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+            map[t.charAt(i)]++;
         }
+
         int have = 0;
-        int need = map.size();
+        int need = 0;
+        for (int i = 0; i < 128; i++) {
+            if (map[i] > 0) {
+                need++;
+            }
+        }
+
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
-            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) - 1);
-            if (map.get(s.charAt(right)) == 0) {
+            char c = s.charAt(right);
+            map[c]--;
+            if (map[c] == 0) {
                 have++;
             }
             while (have == need) {
@@ -21,13 +31,15 @@ class Solution {
                     lIdx = left;
                     rIdx = right;
                 }
-                map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
-                if (map.get(s.charAt(left)) > 0) {
+                char leftChar = s.charAt(left);
+                map[leftChar]++;
+                if (map[leftChar] > 0) {
                     have--;
                 }
                 left++;
             }
         }
+
         return length == Integer.MAX_VALUE ? "" : s.substring(lIdx, rIdx + 1);
     }
 }
